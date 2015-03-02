a04aa669d7cd74d069a3b1f88df32769f230ea31
Victor <melnikukr@mail.ru>
2015-03-02 17:09:03 +0200

    Add indeterminate state to PaperProgressBar

--- library/src/main/java/com/annimon/paperstyle/PaperProgressBar.java
+++ library/src/main/java/com/annimon/paperstyle/PaperProgressBar.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
 import android.widget.ProgressBar;
 
@@ -13,12 +15,14 @@ import android.widget.ProgressBar;
  */
 public class PaperProgressBar extends ProgressBar {
 
+    private static final int ANIMATION_DELAY = 20;
     private static final int GRAY_LINE_COLOR = 0xFFC8C8C8;
 
     private final Paint mColorPaint = new Paint();
 
     private int mProgressColor;
     private int mSecondaryProgressColor;
+    private float mIndeterminateProgress;
 
     public PaperProgressBar(Context context) {
         this(context, null);
@@ -45,6 +49,11 @@ public class PaperProgressBar extends ProgressBar {
         mColorPaint.setColor(mProgressColor);
         mColorPaint.setStyle(Paint.Style.FILL);
         mColorPaint.setAntiAlias(true);
+
+        mIndeterminateProgress = 0;
+        if (isIndeterminate()) {
+            mIndeterminateHandler.sendEmptyMessage(0);
+        }
     }
 
     public int getProgressColor() {
@@ -66,6 +75,17 @@ public class PaperProgressBar extends ProgressBar {
     }
 
     @Override
+    public synchronized void setIndeterminate(boolean indeterminate) {
+        super.setIndeterminate(indeterminate);
+        if (mIndeterminateHandler == null) return;
+        if (indeterminate) {
+            mIndeterminateHandler.sendEmptyMessage(0);
+        } else {
+            mIndeterminateHandler.removeMessages(0);
+        }
+    }
+
+    @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         mColorPaint.setStrokeWidth(h / 3f);
@@ -76,6 +96,16 @@ public class PaperProgressBar extends ProgressBar {
         final float width = getWidth(), h2 = getHeight() / 2;
         mColorPaint.setColor(GRAY_LINE_COLOR);
         canvas.drawLine(0, h2, width, h2, mColorPaint);
+
+        if (isIndeterminate()) {
+            final float scaleX = 0.3f * (50 - Math.abs(mIndeterminateProgress - 50)) / 50f;
+            final float lineX = mIndeterminateProgress * width / 100f;
+            final float lineWidth = width * scaleX / 2;
+            mColorPaint.setColor(mProgressColor);
+            canvas.drawLine(lineX - lineWidth, h2, lineX + lineWidth, h2, mColorPaint);
+            return;
+        }
+
         if (getSecondaryProgress() > 0) {
             final float lineWidth = getSecondaryProgress() * width / getMax();
             mColorPaint.setColor(mSecondaryProgressColor);
@@ -87,4 +117,19 @@ public class PaperProgressBar extends ProgressBar {
             canvas.drawLine(0, h2, lineWidth, h2, mColorPaint);
         }
     }
+
+    private final Handler mIndeterminateHandler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+            mIndeterminateProgress++;
+            if (mIndeterminateProgress >= 100) {
+                mIndeterminateProgress = 0;
+            }
+            invalidate();
+            if (isIndeterminate()) {
+                mIndeterminateHandler.sendEmptyMessageDelayed(0, ANIMATION_DELAY);
+            }
+        }
+    };
 }
